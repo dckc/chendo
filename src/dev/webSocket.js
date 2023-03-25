f10adde3c2ce7b2c96cfe5c81cec716a7d925ec3
Dan Connolly <dckc@madmode.com>
2023-03-25 18:33:34 -0500

    feat: echo svc

--- src/dev/webSocket.js
+++ src/dev/webSocket.js
@@ -1,12 +1,12 @@
 // @ts-check
 /* global assert */
-import { Far } from "@endo/far";
+import { E, Far } from "@endo/far";
 // import { assert } from "ses/src/error/assert.js";
 import { WebSocketServer } from "ws";
 
 /**
  * @typedef {string | Uint8Array} OutMessage
- * @typedef {Buffer | ArrayBuffer | Buffer[]} InMessage
+ * @typedef {string} InMessage
  * @typedef {{ send: (data: OutMessage) => void }} SocketSender
  * @typedef {{
  *   message: (msg: InMessage) => void,
@@ -18,20 +18,29 @@ export const main0 = (_powers) =>
   Far("WebSockets", {
     /** @param {number} port */
     makeServer: (port) => {
+      console.log("makeServer", port);
       assert.typeof(port, "number");
       return Far("WebSocketServer", {
         /**
-         * @param {(sender: SocketSender) => SocketReceiver} onConnection
+         * @param {{ connect: (sender: SocketSender) => SocketReceiver }} handler
          */
-        async listen(onConnection) {
+        async listen(handler) {
+          console.log("listen", handler);
           const wss = new WebSocketServer({ port });
           wss.on("connection", (ws) => {
             const sender = Far("SocketSender", {
-              send: (msg) => ws.send(msg),
+              send: (msg) => {
+                console.log("SocketSender: send", msg);
+                ws.send(msg);
+              },
             });
-            const extSocket = onConnection(sender);
-            ws.on("error", (err) => extSocket.error(err));
-            ws.on("message", (msg) => extSocket.message(msg));
+            const extSocket = E(handler).connect(sender);
+            ws.on("error", (err) => E(extSocket).error(err));
+            ws.on("message", (msg) => {
+              const txt = msg.toString();
+              console.log("@@@ws message:", msg, "as", txt);
+              return E(extSocket).message(txt);
+            });
           });
         },
       });
--- src/echo.js
+++ src/echo.js
@@ -1,20 +1,23 @@
 // @ts-check
 import { E, Far } from "@endo/far";
-import { makePromiseKit } from "@endo/promise-kit";
 
 export const main0 = (powers) => {
   return Far("EchoService", {
     // TODO: type for wss
     async startOn(wss) {
-      const { promise: senderP, resolve } = makePromiseKit();
-      const onConnection = Far("onConnection", (sender) => {
-        resolve(sender);
-        return Far("socket", {
-          error: (err) => console.error(err),
-          message: (msg) => E(senderP).send(msg),
-        });
+      const handler = Far("handler", {
+        connect: (sender) => {
+          console.log("connect:", sender);
+          return Far("socket", {
+            error: (err) => console.error(err),
+            message: (msg) => {
+              console.log("handling msg by echo:", msg);
+              return E(sender).send(msg);
+            },
+          });
+        },
       });
-      await E(wss).listen(onConnection);
+      await E(wss).listen(handler);
       return true;
     },
   });
